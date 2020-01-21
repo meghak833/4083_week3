b4f27280d7d256aa6cc44ed42d503eac726a4771
Nirali Jayswal <njayswal96@gmail.com>
2020-01-21 14:38:08 -0500

    CardTrick created

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,22 +10,29 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * Modified by Nirali Jayswal
+ * Student ID:991500462
+ * 
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int)(Math.random()*13 + 1));
+            c.setSuit(Card.SUITS[(int)(Math.random()* 3 + 0)]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+  
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
-        
         //Then report the result here
     }
     
