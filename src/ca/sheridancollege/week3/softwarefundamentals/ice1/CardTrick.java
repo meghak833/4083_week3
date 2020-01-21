797265f4470d9817f25305b9122be8a340083a80
aeedaq <aeedabq@gmail.com>
2020-01-21 14:38:45 -0500

    Aida

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -16,10 +16,28 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        String card = null;
+ 
         
         for (int i=0; i<magicHand.length; i++)
         {
+            int a = 1 + (int)(Math.random() * 4);  
+        if(a==1)
+            card="Hearts";
+        else if (a==2)
+            card="Diamonds";
+        else if (a==3)
+            card="Spades";
+        else if (a==4)
+            card="Clubs";
+        int b = 1 + (int)(Math.random() * 13);
+        
             Card c = new Card();
+            String[] cardNum= new String[7];
+            int[] cardVal= new int[7];
+            cardNum[i]=card;
+            cardVal[i]=b;
+            System.out.println("Your Card is: "+cardNum[i]+" "+cardVal[i]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
