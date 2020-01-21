2215d3feb665c1398ae9d7538a2c472ebd4aa9ad
Andres <munevarm@sheridancollege.ca>
2020-01-21 14:39:04 -0500

    Random card number and random suit number added

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,9 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Modified by: Andres Munevar
+ * Student ID: 991568888
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+import static java.lang.Math.random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -19,9 +22,29 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+           Card c = new Card();
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int randomCardNum = (int)(ceil(random()*13));
+            c.setValue(randomCardNum);
+            magicHand[i] = c;        
+            //setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int randomNum = (int)(floor(random()*4));
+            c.setSuit(Card.SUITS[randomNum]);
+            switch (randomNum) {
+                case 0:
+                    System.out.println("Hearths");
+                    break;
+                case 1:
+                    System.out.println("Diamonds");
+                    break;
+                case 2:
+                    System.out.println("Spades");
+                    break;
+                default:
+                    System.out.println("Clubs");
+                    break;
+            }
+           
         }
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
