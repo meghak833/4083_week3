95de798b9c527715b8168a9778987439419dafd7
Khushi Patel <pate2054@sheridancollege.ca>
2020-01-21 14:39:29 -0500

    for loop modified to generate random card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ *  Modified By :Khushi Paetl(991560930)
  */
 public class CardTrick {
     
@@ -20,10 +21,12 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*3+0)]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
-        
+       
         //insert code to ask the user for Card value and  or Hard code it, create their card
         
         //Then report the result here
