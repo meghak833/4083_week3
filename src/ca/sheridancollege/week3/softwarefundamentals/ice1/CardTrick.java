88a8631fa25d29e43986a16bb816b960f224583f
megha <megha.patel2@sheridancollege.ca>
2020-01-21 13:40:36 -0500

    commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -24,8 +24,8 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        //insert code to ask the user for Card value and  or Hard code it, create their card
+        
         //Then report the result here
     }
     
