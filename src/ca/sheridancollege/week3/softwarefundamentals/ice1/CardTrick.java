ce5d678eebcd6d020c9671e58515ad9b53a7ea2a
aeedaq <aeedabq@gmail.com>
2020-01-21 22:58:38 -0500

    This is my improved version of program.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,31 +18,37 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        String card = null;
  
         
         for (int i=0; i<magicHand.length; i++)
         {
-            int a = 1 + (int)(Math.random() * 4);  
-        if(a==1)
-            card="Hearts";
-        else if (a==2)
-            card="Diamonds";
-        else if (a==3)
-            card="Spades";
-        else if (a==4)
-            card="Clubs";
-        int b = 1 + (int)(Math.random() * 13);
-        
+            
             Card c = new Card();
-            String[] cardNum= new String[7];
-            int[] cardVal= new int[7];
-            cardNum[i]=card;
-            cardVal[i]=b;
-            System.out.println("Your Card is: "+cardNum[i]+" "+cardVal[i]);
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            c.setValue((int)(1+(Math.random()*13)));
+            c.setSuit(Card.SUITS[(int)((Math.random()*4))]);
+            
+            magicHand[i]=c;
+            
+            System.out.println("The Card in Magic hand is " + magicHand[i].getValue()+" "+magicHand[i].getSuit());
         }
+        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pick a number");
+        int num = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Pick a suit");
+        String val = scan.nextLine();
+        
+        
+        
+        Card myCard = new Card();
+        myCard.setSuit(val);
+        myCard.setValue(num);
+        
+        System.out.println("My Card is " + myCard.getSuit()+" "+myCard.getValue());
+        
         
         //insert code to ask the user for Card value and  or Hard code it, create their card
         
